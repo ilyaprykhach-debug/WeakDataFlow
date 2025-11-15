9e6d883f23017bc57f602893c1fbddb9e9447d4a
ilyaprykhach-debug <ilya.prykhach@innowise.com>
2025-11-15 21:36:38 +0100

    Adding docker-compose and removing useless logs

--- DataProcessor.Service/Services/DatabaseService.cs
+++ DataProcessor.Service/Services/DatabaseService.cs
@@ -1,7 +1,6 @@
 using DataProcessor.Service.Data;
 using DataProcessor.Service.Interfaces;
 using DataProcessor.Service.Models;
-using DataProcessor.Service.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataProcessor.Service.Services;
@@ -41,50 +40,31 @@ public class DatabaseService : IDatabaseService
     {
         try
         {
-            _logger.LogInformation("Start SaveSensorReadingsBatchAsync");
-
             var readingsList = readings.ToList();
             var ids = readingsList.Select(r => r.Id).ToList();
-
-            _logger.LogInformation("Reading list created");
 
             var existingReadings = await _context.SensorReadings
                 .Where(r => ids.Contains(r.Id))
                 .ToListAsync(cancellationToken);
 
-            _logger.LogInformation("existingReadings created");
-
             var existingIds = existingReadings.Select(r => r.Id).ToHashSet();
-
-            _logger.LogInformation("existingIds created");
-
 
             foreach (var reading in readingsList)
             {
-                _logger.LogInformation("Start foreach");
-
                 if (existingIds.Contains(reading.Id))
                 {
-                    _logger.LogInformation("Ids contained");
-
                     var existing = existingReadings.First(r => r.Id == reading.Id);
                     _context.Entry(existing).CurrentValues.SetValues(reading);
-                    _logger.LogInformation("Set values");
-
                 }
                 else
                 {
-                    _logger.LogInformation("Adding to db");
-
                     await _context.SensorReadings.AddAsync(reading, cancellationToken);
-
                     _logger.LogInformation("Data added to db");
 
                 }
             }
 
             await _context.SaveChangesAsync(cancellationToken);
-            _logger.LogInformation("Batch of {Count} sensor readings saved", readingsList.Count);
 
             if (_notificationClient != null)
             {
